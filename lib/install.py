e7f1ecd8713272e8aa40aed357f5efd14baa1b93
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-08-16 23:39:14 -0500

    Install GHC using Python

--- lib/install.py
+++ lib/install.py
@@ -1,37 +1,75 @@
+from functools import reduce
+
+import csv
+import glob
 import gzip
 import os
 import platform
 import re
 import subprocess
+import sys
+import tarfile
+import tempfile
 import urllib.request
 
-bar = {
-  'Debian': 'deb'
+NAMES = {
+  'debian': 'deb'
 }
 
-def install(path, version):
-    urls = assets_urls(version)
-    print(list(urls))
-    os.mkdir(f"{path}/bin")
-    return f"{path}/bin"
+def install(install_dir, version):
+  urls = __compatible_urls(__downloadable_urls(version))
+  info = __distro()
+  url = __specific_url(urls, info)
+  return __install_from_url(install_dir, version, url)
 
-def assets_urls(version, system = platform.system(), machine = platform.machine()):
-    url = f"https://downloads.haskell.org/~ghc/{version}/"
-    system = system.lower()
-    machine = machine.lower()
+def __specific_url(urls, info):
+  filtered_urls = filter(lambda url: f'{info[0]}{info[1]}' in url and url.endswith('tar.xz'), urls)
+  return next(filtered_urls, None)
 
-    def url_and_tags(name):
-        tags = name.replace('.tar.xz', '').split('-')[2::]
-        tags.remove(system)
-        tags.remove(machine)
-        return (f"{url}{name}", tags)
+def __install_from_url(install_dir, version, url):
+  with tempfile.TemporaryDirectory() as download_dir:
+    path, _ = urllib.request.urlretrieve(url, f"{download_dir}/ghc.tar.xz")
+    with tarfile.open(path) as tar:
+      tar.extractall(download_dir)
+      working_dir = f'{download_dir}/ghc-{version}'
+      subprocess.run(['./configure', f'--prefix={install_dir}'], cwd=working_dir)
+      subprocess.run(['make', 'install'], cwd=working_dir)
+      return True
 
-    with urllib.request.urlopen(url) as res:
-        content = res.read().decode('utf-8')
-        return map(
-            url_and_tags,
-            filter(
-                lambda url: system in url and machine in url,
-                re.findall(f"\"(ghc-{version}-.*\.tar\.xz)\"", content)
-            )
-        )
+def __distro():
+  info = __os_release()
+  return (NAMES[info['ID']], int(info['VERSION_ID']))
+
+def __os_release():
+  def row_to_info(info, row):
+    info[row[0]] = row[1]
+    return info
+
+  with open('/etc/os-release') as f:
+    return reduce(row_to_info, csv.reader(f, delimiter='='), {})
+
+def __compatible_urls(urls):
+  arch = platform.machine()
+  os = sys.platform
+  return filter(lambda url: arch in url and os in url, urls);
+
+def __downloadable_urls(version):
+  base_url = f'https://downloads.haskell.org/~ghc/{version}/'
+  with urllib.request.urlopen(base_url) as resp:
+    html = gzip.decompress(resp.read()).decode('utf-8')
+    targets = re.findall('href="(.*)"', html)
+    return map(
+      lambda filename: f'{base_url}{filename}',
+      filter(__is_downlodable(version), targets)
+    )
+
+def __is_downlodable(version):
+  def zoo(target):
+    return target.startswith(f'ghc-{version}') and not target.endswith('.sig')
+
+  return zoo
+
+if __name__ == '__main__':
+  install_dir = os.environ['ASDF_INSTALL_PATH']
+  version = os.environ['ASDF_INSTALL_VERSION']
+  install(install_dir, version)
--- test/test_install.py
+++ test/test_install.py
@@ -1,21 +1,12 @@
-import lib.install
+from lib.install import install
+
 import subprocess
 import tempfile
 import unittest
 
 class TestInstall(unittest.TestCase):
-    # def test_install(self):
-        # with tempfile.TemporaryDirectory() as tmpdir:
-            # bin_dir = lib.install.install(tmpdir, '8.8.1')
-            # version = subprocess.check_output([f"{bin_dir}/ghc", '--version'])
-            # self.assertIn(b'8.8.1', version)
-    def test_assets_urls(self):
-        url = 'https://downloads.haskell.org/~ghc/8.8.1'
-        urls = list(lib.install.assets_urls('8.8.1', 'linux', 'x86_64'))
-        self.assertEqual(urls, [
-            (f"{url}/ghc-8.8.1-x86_64-centos7-linux.tar.xz", ['centos7']),
-            (f"{url}/ghc-8.8.1-x86_64-deb8-linux.tar.xz", ['deb8']),
-            (f"{url}/ghc-8.8.1-x86_64-deb9-linux-dwarf.tar.xz", ['deb9', 'dwarf']),
-            (f"{url}/ghc-8.8.1-x86_64-deb9-linux.tar.xz", ['deb9']),
-            (f"{url}/ghc-8.8.1-x86_64-fedora27-linux.tar.xz", ['fedora27'])
-        ])
+  def test_install(self):
+    with tempfile.TemporaryDirectory() as install_dir:
+      install(install_dir, '8.10.1')
+      output = subprocess.check_output([f'{install_dir}/bin/ghc', '--version'])
+      self.assertIn(b'8.10.1', output)
