86a63db7dd0776155bb27fefa6daacb16d5f0a3b
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-09-17 12:06:59 -0500

    Install GHC

--- lib/install.py
+++ lib/install.py
@@ -4,17 +4,39 @@ import platform
 import re
 import subprocess
 import sys
+import tarfile
+import tempfile
 import urllib.request
 
 ARCH = platform.machine()
-BASE_URL = 'https://downloads.haskell.org/~ghc/'
+BASE_URL = 'https://downloads.haskell.org/~ghc'
 OS = sys.platform
 
 
 def install(install_dir, version):
-    filenames = __filter_by_distro(__get_filenames(version))
-    print(list(filenames))
-    return None
+    filename = __downloadable_filename(
+      __sort_by_distro_version(__filter_by_distro(__get_filenames(version)))
+    )
+    return __download_and_install(
+      install_dir,
+      version,
+      f"{BASE_URL}/{version}/{filename['filename']}"
+    )
+
+
+def __downloadable_filename(filenames):
+  def no_extras(filename):
+    return len(filename['extras']) == 0
+
+  return next(filter(no_extras, filenames))
+
+
+def __sort_by_distro_version(filenames):
+  def by_distro_version(filename):
+    return filename['distro']['version']
+
+  return sorted(filenames, key=by_distro_version, reverse=True)
+
 
 def __filter_by_distro(filenames):
     distro = __normalize_distro(subprocess.check_output(['lsb_release', '-irs']))
@@ -34,7 +56,7 @@ def __get_filenames(version):
     def is_tarball(filename):
       return ARCH in filename and OS in filename and filename.endswith('.tar.xz')
 
-    with urllib.request.urlopen(f'{BASE_URL}{version}/') as resp:
+    with urllib.request.urlopen(f'{BASE_URL}/{version}/') as resp:
         content = resp.read().decode('utf-8')
         return map(
           __parse_filename,
@@ -54,6 +76,16 @@ def __parse_filename(filename):
 def __parse_distro(distro):
     values = next(iter(re.findall('([a-z]+)(\d+)', distro)))
     return { 'name': values[0], 'version': float(values[1]) }
+
+
+def __download_and_install(install_dir, version, url):
+  with tempfile.TemporaryDirectory() as download_dir:
+    path, _ = urllib.request.urlretrieve(url, f"{download_dir}/ghc.tar.xz")
+    with tarfile.open(path) as tar:
+      tar.extractall(download_dir)
+      working_dir = f'{download_dir}/ghc-{version}'
+      subprocess.run(['./configure', f'--prefix={install_dir}'], cwd=working_dir)
+      subprocess.run(['make', 'install'], cwd=working_dir)
 
 
 if __name__ == '__main__':
