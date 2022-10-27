c71fdcb58cb4d48dd276231086f89a23a78eb4cc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 19:01:29 +0000

    Adding tarfile member sanitization to extractall()

--- lib/install.py
+++ lib/install.py
@@ -75,7 +75,26 @@ def __install(install_dir, version, url):
             '%s/ghc.tar.xz' % download_dir
         )
         with tarfile.open(path) as tar:
-            tar.extractall(download_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, download_dir)
             working_dir = '%s/ghc-%s' % (download_dir, version)
             subprocess.run(
                 ['./configure', '--prefix=%s' % install_dir],
